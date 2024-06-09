9b5fc01ccb4cedb3ef35aacdf08e588a279478c0
alvarosena <alvarosenacs.c@gmail.com>
2024-06-09 17:58:31 -0300

    dynamic navbar

--- js/header.js
+++ js/header.js
@@ -1,3 +1,55 @@
+function render() {
+  const pathname = window.location.pathname + window.location.hash
+
+  const links = [
+    {
+      id: 'home-link',
+      href: pathname.includes('atelie-buguela') ? '/atelie-buguela' : '/',
+      content: 'Início',
+    },
+    {
+      id: 'products-link',
+      href: pathname.includes('atelie-buguela') ? '/atelie-buguela/#produtos' : '/#produtos', 
+      content: 'Produtos',
+    },
+    {
+      id: 'about-link',
+      href: pathname.includes('atelie-buguela') ? '/atelie-buguela/#quem-somos' : '/#quem-somos',
+      content: 'Quem somos?',
+    },
+    {
+      id: 'contact-link',
+      href:  pathname.includes('atelie-buguela') ? '/atelie-buguela/#contato' : '/#contato',
+      content: 'Contato',
+    },
+    {
+      id: 'blog-link',
+      href:  pathname.includes('atelie-buguela') ? '/atelie-buguela/blog/index.html' : '/blog/index.html',
+      content: 'Blog',
+    },
+  ]
+
+  let contentBody = ''
+
+  links.forEach(link => {
+    console.log(link)
+
+    contentBody += `
+      <a 
+        id="${link.id}"
+        href="${link.href}"
+        class="text-zinc-950 active:text-orange-500 leading-[80px]"
+        >
+        ${link.content}
+        </a>
+      `
+  })
+
+  document.getElementById('desktop-nav').innerHTML = contentBody
+}
+
+render()
+
 window.addEventListener('scroll', () => {
   const header = document.getElementById('main-header')
   if (window.scrollY > 0) {
