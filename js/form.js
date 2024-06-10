0df36bcf5d83bb2fc98ac4d1e0547a182dc397b2
alvarosena <alvarosenacs.c@gmail.com>
2024-06-10 09:55:30 -0300

    add favicon and fixed success page link

--- js/form.js
+++ js/form.js
@@ -2,9 +2,13 @@ const currentLocation = window.location.href
 const prodLocation = 'https://alvarosena.github.io/atelie-buguela'
 
 const redirectField = document.getElementById('redirect-form')
+const backPageLink = document.getElementById('a-back-home')
+console.log(backPageLink.href)
 
 if (currentLocation === prodLocation) {
   redirectField.value = `${prodLocation}/success/index.html`
+  backPageLink.href = `${prodLocation}`
 } 
 
 redirectField.value = `${currentLocation}success/index.html`
+backPageLink.href = `${currentLocation}`
