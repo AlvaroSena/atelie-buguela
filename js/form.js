a449080a85309a43b2d6b3a4c2e62e34da2e8ae5
alvarosena <alvarosenacs.c@gmail.com>
2024-06-10 10:06:01 -0300

    removed a link at success.html

--- js/form.js
+++ js/form.js
@@ -2,12 +2,10 @@ const currentLocation = window.location.href
 const prodLocation = 'https://alvarosena.github.io/atelie-buguela'
 
 const redirectField = document.getElementById('redirect-form')
-const backPageLink = document.getElementById('a-back-home')
 
 if (currentLocation.includes(prodLocation)) {
   redirectField.value = `${prodLocation}/success/index.html`
   backPageLink.href = `${prodLocation}`
 } else {
   redirectField.value = `${currentLocation}success/index.html`
-  backPageLink.href = `${currentLocation}`
 }
