b23246d79ffea6418e593ab73f9168fbd92fd76a
alvarosena <alvarosenacs.c@gmail.com>
2024-06-10 10:02:20 -0300

    change at form.js

--- js/form.js
+++ js/form.js
@@ -3,12 +3,11 @@ const prodLocation = 'https://alvarosena.github.io/atelie-buguela'
 
 const redirectField = document.getElementById('redirect-form')
 const backPageLink = document.getElementById('a-back-home')
-console.log(backPageLink.href)
 
-if (currentLocation === prodLocation) {
+if (currentLocation.includes(prodLocation)) {
   redirectField.value = `${prodLocation}/success/index.html`
   backPageLink.href = `${prodLocation}`
-} 
-
-redirectField.value = `${currentLocation}success/index.html`
-backPageLink.href = `${currentLocation}`
+} else {
+  redirectField.value = `${currentLocation}success/index.html`
+  backPageLink.href = `${currentLocation}`
+}
